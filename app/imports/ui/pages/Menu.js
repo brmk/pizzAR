cfdd90f86e48f457ce367595c53c7eb393b196ac
Yura Srohiy <yura.srohiy@gmail.com>
2017-10-08 04:15:53 +0300

    working on menu

--- app/imports/ui/pages/Menu.js
+++ app/imports/ui/pages/Menu.js
@@ -1,7 +1,8 @@
 //----
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { Meteor } from 'meteor/meteor';
+import { Meteor }  from 'meteor/meteor';
+import {Session} from 'meteor/session'
 import { createContainer } from 'meteor/react-meteor-data';
 import {_} from 'lodash';
 // import AR from 'com.wikitude.phonegap.WikitudePlugin';
@@ -18,19 +19,22 @@ let pizzas = {
     name: "Capricciosa",
     price: 60,
     ingridients:["sause", "cheese", "ham", "mushrooms"],
-    weigtht: 500
+    weight: 500,
+    image: "http://www.seriouseats.com/images/2017/04/20170411-pizza-oven-testing-roccbox-top.jpg"
   },
   margarita:{
     name: "Margarita",
     price: 30,
     ingridients:["sause", "cheese"],
-    weigtht: 450
+    weight: 450,
+    image: "http://www.seriouseats.com/images/2017/04/20170411-pizza-oven-testing-roccbox-top.jpg"
   },
   hawaian:{
     name: "Hawaian",
     price: 65,
     ingridients: ["sause", "ham", "pineapple", "cheese"],
-    weigtht: 550
+    weight: 550,
+    image: "http://www.seriouseats.com/images/2017/04/20170411-pizza-oven-testing-roccbox-top.jpg"
   }
 }
 
@@ -40,7 +44,11 @@ class Menu extends Component {
     super(props);
   
     this.state = {
-
+      sizes:{
+        capricciosa: 1,
+        margarita: 1,
+        hawaian: 1
+      }
     };
   }
   
@@ -59,24 +67,59 @@ class Menu extends Component {
   render(){
     return (
       <Ons.Page renderToolbar={this.renderToolbar}>
-        {_.map(pizzas, (pizza, key)=>{
-          return (<Ons.Col width="50%">
+        <Ons.Row>
+          {_.map(pizzas, (pizza, key)=>{
+            return (
+              <Ons.Col key={key} width="50%">
+                <Ons.Card verticalAlign>
+                  <img className="img-responsive" src={pizza.image}/>
+                  <Ons.Row>
+                    <Ons.Col width="80%">
+                      <div>
+                        <h3>{pizza.name}</h3>
+                        <h5>{pizza.price} UAH</h5>
+                        <h5>{pizza.weight} g</h5>
+                      </div>
+                    </Ons.Col>
+                    <Ons.Col width="20%">
+                      <Ons.Icon onClick={()=>{
+                        let cart = Session.get('cart');
+                        if(!Array.isArray(cart)){
+                          cart = []
+                        }
+                        cart.push({
+                          name: pizza.name,
+                          price: pizza.price * this.state.sizes[key],
+                          weight: pizza.weight * this.state.sizes[key],
+                          img: pizza.image
+                        })
+                        Session.set('cart', cart)
+                        console.log(cart)
+                      }} icon='fa-shopping-cart' size={30}/>
+                    </Ons.Col>
+                  </Ons.Row>
+                </Ons.Card>
+              </Ons.Col>
+            )
+          })}
+
+          <Ons.Col width="50%">
             <Ons.Card verticalAlign>
-              <img className="img-responsive" src="http://www.seriouseats.com/images/2017/04/20170411-pizza-oven-testing-roccbox-top.jpg"/>
-              <Ons.Col width="80%">
-                <div>
-                  <h3>{pizza.name}</h3>
-                  <h4>{pizza.price} UAH</h4>
-                </div>
-              </Ons.Col>
-              <Ons.Col width="20%">
-                <Ons.Icon icon='fa-shopping-cart' />
-              </Ons.Col>
-              
+              <img className="img-responsive" src="https://image.freepik.com/free-icon/add-filled-cross-sign_318-75178.jpg"/>
+              <Ons.Row>
+                <Ons.Col width="100%">
+                  <div>
+                    <br/>
+                    <br/>
+                    <Ons.Button style={{margin: '6px'}} modifier='large'>BUILD PIZZA</Ons.Button>
+                    <br/>
+                  </div>
+                </Ons.Col>
+              </Ons.Row>
             </Ons.Card>
-          </Ons.Col>)
-          
-        })}
+          </Ons.Col>
+        </Ons.Row>
+        
 
       </Ons.Page>
     );
